29197145ae8749ff7423d9a1fde6b3e593147bcd
UraN9 <kwuvezzzkuy@gmail.com>
2024-10-22 09:52:07 +0300

    Почали роботу з класами

--- 3_lab/my_class.py
+++ 3_lab/my_class.py
@@ -1,5 +1,16 @@
-class My_class:
-    print("Завантажились")
+class MyClass:
+    def __init__(self, surname:str, name:str, mark:int): # у конструктор передаються аргументи
+        print("Викликали конструктор!")
+        # а тут визначаємо атрибути об`єкта
+        self.surname = surname
+        self.name = name
+        self.mark = mark
+        #self.penalty = None # можеми записати динамічний атрибут тут, але він перстане бути динамічним
+        print("Завершили присвоєння атрибутів")
 
-class My_second_class:
+    
+    def __repr__(self):
+        return f"MyClass(Ніяких аргументів не переаємо)"
+
+class MySecondClass:
     pass
